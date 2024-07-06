7cf2f488e01587547d8b81df81ceff278d306c6b
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-07-06 14:58:30 +0530

    Update shinchan.js

--- shinchan.js
+++ shinchan.js
@@ -1,1 +1,7 @@
-
+function getListValue() {
+var selectedOption, st;
+selectedOption = document.getElementById("myList").selectedIndex;
+st=document.getElementById("myList").options[selectedOption].value
+//alert(st);
+document.getElementById("myTextBoxResult").value=st;
+}
